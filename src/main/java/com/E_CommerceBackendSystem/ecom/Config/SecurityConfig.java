10950fb3b8d9102487c75a47274e14f54a5afd8a
Om Phopse <omphopse19@gmail.com>
2026-03-07 10:51:47 +0530

    added notes

--- src/main/java/com/E_CommerceBackendSystem/ecom/Config/SecurityConfig.java
+++ src/main/java/com/E_CommerceBackendSystem/ecom/Config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.E_CommerceBackendSystem.ecom.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//HttpSecurity is used to configure web security rules.
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+// Request → Authentication Filter → Authorization Filter → Controller
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -13,6 +15,11 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		 http
          .authorizeHttpRequests(auth -> auth
+//        		 auth represents:
+//        			 AuthorizationManagerRequestMatcherRegistry
+//        		 Which basically means:
+//        			 object used to define URL access rules
+        		 
         	  .requestMatchers("/user/**").hasRole("USER")
         	  .requestMatchers("/admin/**").hasRole("ADMIN")
               .requestMatchers("/public/**").permitAll()
@@ -23,7 +30,8 @@ public class SecurityConfig {
 		 http
 		    .csrf(csrf -> csrf.disable())
 		    .httpBasic(withDefaults());
-     return http.build();
+		 
+		 return http.build();
 	}
 	
 	@Bean
